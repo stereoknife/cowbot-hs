9ccbe07f4c0b6189add5d86e11278f130d30777f
nil <git@garbage.techniworm.dev>
2020-11-15 22:43:33 +0200

    nettis 2

--- src/Bot/Internal.hs
+++ src/Bot/Internal.hs
@@ -6,10 +6,12 @@ module Bot.Internal ( Permission (..)
              , ReactionData (..)
              , Command
              , Reaction
+             , Net
              ) where
 
 import           Bot.Internal.Command (Permission (..))
 import           Bot.Internal.Data    (MessageData (..), ReactionData (..))
 import           Bot.Internal.Discord (Command, DiscordRequest (..), Reaction)
+import           Bot.Internal.Net     (Net)
 import           Bot.Internal.Parser  (Parser (..))
 import           Bot.Internal.Reply   (Reply (..))
