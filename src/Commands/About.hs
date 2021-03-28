8e18f29332d7728eca0c0b20a7febf9fbf4cd03d
nil <git@garbage.techniworm.dev>
2021-03-28 14:24:05 +0300

    some about correction

--- src/Commands/About.hs
+++ src/Commands/About.hs
@@ -6,7 +6,7 @@ import           Network.Discord (Reply (reply))
 
 about :: Command ()
 about = do
-    reply $ "**Cowbot** "
-         <> "version `3.0.1` \n\n"
+    reply $ "**Cowbot 🤠** "
+         <> "version `3.0.0` \n\n"
          <> "Git repository: <https://github.com/stereoknife/cowbot-hs> \n"
          <> "Icon by <https://twitter.com/tdtbaa/>"
