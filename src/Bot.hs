c84776adfe7f48de0e4d024282ee7f1832d0d748
kam <git@snaek.anonaddy.com>
2021-10-20 23:08:11 +0300

    haha tests

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Bot where
 import Data.Text (Text)
 import Discord (DiscordHandler)
 import Discord.Types (Message (messageText))
 import qualified Data.Text as T
 import Control.Monad (guard)
-import Howdy.Actions ( Command, Reply(reply), command )
 import Control.Monad.Trans.Reader ( asks )
+import Howdy.Bot (run, command, prefix)
+import Howdy.Parser (word)
+import Howdy.Actions (ActionContext, Reply (reply))
+import Howdy.Context (Context(fctx))
 
 -- howdy monad is parser + discordhandler
 
@@ -16,6 +20,39 @@ run = bot $ -- bot is function that runs discord with preconfigured settings
   reaction -- reactions are specified from messages reacted to
 -}
 
-echo :: Command ()
-echo = command
-     $ asks messageText >>= reply
+-- Option 1
+
+
+bot :: a
+bot = undefined 
+
+cowbot :: Monad m => m ()
+cowbot = bot $ do
+  prefix ["boy howdy", "cowboy"]
+
+  command
+    "bless"
+    "blesses the chat"
+    bless
+
+  command
+    "clap"
+    "replaces spaces with claps"
+    clap
+
+
+bless :: ActionContext m => m ()
+bless = do
+  -- fetch bless data from api
+  reply "here is the bless"
+
+clap :: ActionContext m => m ()
+clap = do
+  a <- fctx argString
+  reply . T.intercalate "clap" . T.words $ a
+
+-- command end: needs these
+-- how to handle aliases?
+
+argString :: Message -> Text
+argString = undefined 
