be99144b695f7e9dec91d36b0749c335e0e3e58d
kam <11618271+stereoknife@users.noreply.github.com>
2021-11-09 18:30:40 +0200

    Yeet all of Howdy out, make bot skeleton

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-# Changelog for cowbot-stack
+# Revision history for cowbot
 
-## Unreleased changes
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,49 @@
 module Main where
 
-import           Bot
+import           Control.Applicative (Alternative (empty, many))
+import qualified Data.Text           as T
+import           Howdy.Action        (action, alias, aliases, desc)
+import           Howdy.Bot           (bot, command, prefixes, run)
+import           Howdy.Discord.Class (Reply (reply))
+import           Howdy.Parser        (MonadParse (parse), char, rest)
 
 main :: IO ()
-main = cowbot
+main = run . bot $ do
+    prefixes ["boy howdy", "🤠"]
+
+    command $ do
+        alias "ping"
+        desc "just a ping"
+        action $ reply "pong"
+        -- disabled
+
+    command $ do
+        alias "echo"
+        desc "no desc"
+        action $ do
+            t <- parse rest
+            case t of Just t' -> reply t'
+                      _       -> pure ()
+
+    command $ do
+        alias "elongate"
+        desc "it elongates"
+        action $ do
+            t <- parse (many (char ' ') >> rest)
+            case t of Just t' -> reply $ "`" <> T.intersperse ' ' t' <> "`"
+                      _       -> pure ()
+
+    command $ do
+        alias "bless"
+        desc "blesses the chat"
+        action empty
+
+    command $ do
+        aliases ["t", "translate"]
+        desc "translates something to english"
+        action empty
+
+    command $ do
+        alias "uwu"
+        desc "uwu"
+        action empty
